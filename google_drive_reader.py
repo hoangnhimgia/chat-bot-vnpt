6a95e37a2377f617d6907d7e43c835061600e4c2
hoangnhimgia <giangthaihoangmacair@gmail.com>
2025-07-22 09:55:03 +0700

    google_drive_reader.py

--- google_drive_reader.py
+++ google_drive_reader.py
@@ -5,3 +5,10 @@ def init_drive_service():
     creds = service_account.Credentials.from_service_account_file("credentials.json")
     service = build("drive", "v3", credentials=creds)
     return service
+def list_files_in_folder(service, folder_id):
+    query = f"'{folder_id}' in parents and trashed = false"
+    results = service.files().list(q=query).execute()
+    files = results.get("files", [])
+    for file in files:
+        print(f"📄 Tên file: {file['name']} — ID: {file['id']}")
+    return files
